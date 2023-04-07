ef6f309b302b7ef1a8219ff923a8c66103e53d11
vinicius-je <vinicius.je99@gmail.com>
2023-04-07 17:14:11 -0300

    padrões de projeto spring

---
+++ padroes-projeto-spring/src/main/java/com/example/padroesprojetospring/service/ClienteService.java
@@ -0,0 +1,17 @@
+package com.example.padroesprojetospring.service;
+
+import com.example.padroesprojetospring.model.Cliente;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ClienteService {
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+}
